565f6b6fb1ba83338e75f36222dd8c12e4253fd8
P-hansen <philip.pt.hansen@gmail.com>
2021-03-12 05:24:09 +0000

    geolocation added

--- iss.js
+++ iss.js
@@ -8,6 +8,7 @@
  */
 const request = require("request");
 const getMyIp = "https://api.ipify.org?format=json";
+const getMyLocation = "https://freegeoip.app/json/";
 
 const fetchMyIP = (callback) => {
   request(getMyIp, (error, response, body) => {
@@ -22,9 +23,29 @@ const fetchMyIP = (callback) => {
       return;
     }
     const adress = JSON.parse(body);
-   // console.log(adress["ip"]);
+    // console.log(adress["ip"]);
     callback(error, adress["ip"]);
   });
-}
+};
 
-module.exports = fetchMyIP;
+const fetchCoordsByIP = (ip, callback) => {
+  request(getMyLocation, ip, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    // if non-200 status, assume server error
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+    const location = JSON.parse(body);
+    callback(error, location);
+  });
+};
+
+module.exports = {
+  fetchCoordsByIP,
+  fetchMyIP
+};
